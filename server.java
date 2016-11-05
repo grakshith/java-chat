eccbd4b8e8580ed1ff30588e326ca6a2363c5970
hehaichi <hehaichi@gmail.com>
2016-11-05 12:46:25 +0530

    Multi-client server

---
+++ server.java
@@ -0,0 +1,214 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.nio.channels.*;
+import java.nio.*;
+import java.util.concurrent.*;
+class Server 
+{
+	ArrayList<Socket> clientArray;
+	ConcurrentLinkedQueue<String> msgQueue;
+	Selector selector;
+	public static void main(String args[])throws IOException
+	{
+		Server serve = new Server();
+		
+	}
+
+	public Server()throws IOException
+	{
+		//this.listen();
+		clientArray=new ArrayList<Socket>();
+		msgQueue= new ConcurrentLinkedQueue<String>();
+		selector = Selector.open();
+		Thread t1 = new Thread(){
+			public void run()
+			{
+				listen();
+			}
+		};
+		Thread t2 = new Thread(){
+			public void run()
+			{
+				try{
+					receiveMessages();
+				}
+				catch(Exception e){}
+			}
+		};
+		Thread t3 = new Thread(){
+			public void run()
+			{
+				try
+				{
+					broadcast();
+				}
+				catch(Exception e){}
+			}
+		};
+		//t1.start();
+		t2.start();
+		t3.start();
+	}
+	public void listen()
+	{
+		try
+		{
+			ServerSocketChannel ssc = ServerSocketChannel.open();
+			ServerSocket sock = ssc.socket();
+     		InetSocketAddress isa = new InetSocketAddress(5555);
+      		sock.bind(isa);
+			while(true)
+			{
+				Socket client = sock.accept();
+				System.out.println(client);
+				clientArray.add(client);
+				SocketChannel sc = client.getChannel();
+				System.out.println(sc);
+				sc.configureBlocking(false);
+				selector.wakeup();
+				sc.register(selector,SelectionKey.OP_READ);
+				System.out.println("New client: "+client.getRemoteSocketAddress().toString());
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void receiveMessages()
+	{
+		System.out.println("Server receiving messages now");
+		ServerSocketChannel ssc;
+		ServerSocket sock=null;
+		try
+		{
+			ssc = ServerSocketChannel.open();
+			ssc.configureBlocking(false);
+			sock = ssc.socket();
+     		InetSocketAddress isa = new InetSocketAddress(5555);
+      		sock.bind(isa);
+      		ssc.register(selector,SelectionKey.OP_ACCEPT);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		while(true)
+		{
+			System.out.println("Pass 1");
+			try
+			{
+				int num=selector.select();
+				if(num==0)
+					continue;
+				Set keys = selector.selectedKeys();
+				Iterator it = keys.iterator();
+				while(it.hasNext())
+				{
+					SelectionKey key = (SelectionKey)it.next();
+					if((key.readyOps() & SelectionKey.OP_ACCEPT) == SelectionKey.OP_ACCEPT)
+					{
+						Socket client = sock.accept();
+						System.out.println(client);
+						clientArray.add(client);
+						SocketChannel sc = client.getChannel();
+						ByteBuffer buff = ByteBuffer.allocate(1024);
+						String message="Hello there";
+						byte[] bytebuff=message.getBytes("UTF-8");
+						buff.put(bytebuff);
+						buff.flip();
+						sc.write(buff);
+						System.out.println(sc);
+						sc.configureBlocking(false);
+						sc.register(selector,SelectionKey.OP_READ);
+						System.out.println("New client: "+client.getRemoteSocketAddress().toString());
+					}
+					if((key.readyOps() & SelectionKey.OP_READ) == SelectionKey.OP_READ)
+					{
+						SocketChannel sc=null;
+						try
+						{
+
+							sc=(SocketChannel)key.channel();
+							Socket client=sc.socket();
+							ByteBuffer buff = ByteBuffer.allocate(1024);
+							int status=sc.read(buff);
+							System.out.println(status);
+							if(status==0 || status==-1)
+							{
+								clientArray.remove(client);
+								sc.close();
+							}
+							//buff.flip();
+							String message=new String(buff.array(),java.nio.charset.StandardCharsets.UTF_8);
+							System.out.println("Reading now");
+							System.out.println(message);
+							msgQueue.add(message);
+							System.out.println("Message added to queue");
+							System.out.println(msgQueue.peek());
+							//broadcast(client.getRemoteSocketAddress().toString(),message);
+
+						}
+						catch(IOException e)
+						{
+							e.printStackTrace();
+							key.cancel();
+							try
+							{
+								sc.close();
+							}
+							catch(IOException e2)
+							{
+								System.err.println("Error closing the socketchannel "+e2);
+							}
+							System.err.println("Exception occurred");
+						}
+					}
+				}
+				keys.clear();
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+	}
+	public void broadcast()
+	{
+		System.out.println("Broadcast started");
+		while(true)
+		{
+			//System.out.println("Iterating");
+			for(Socket sock : clientArray)
+			{
+				try
+				{
+					// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+					// bw.write("Hello");
+					SocketChannel sc = sock.getChannel();
+					ByteBuffer buff = ByteBuffer.allocate(1024);
+					String message=null;
+					message=msgQueue.poll();
+					//System.out.println(message);
+					if(message!="null")
+					{
+						System.out.println(message);
+						byte[] byteBuff=message.getBytes("UTF-8");
+						buff.clear();
+						buff.put(byteBuff);
+						buff.flip();
+						System.out.println("Writing");
+						sc.write(buff);
+						System.out.println("Wrote");
+						System.out.println(sock);
+					}
+				}
+				catch(IOException e){}
+				//sock.send(message);
+			}
+		}
+	}
+}
